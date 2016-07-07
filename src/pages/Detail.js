11cba2821728154e97f402c5fbcfbab8b612c379
seanreed1111 <seanreed1111@gmail.com>
2016-07-06 22:39:39 -0400

    adds buttons and bind(this) and forceUpdate() to Detail component

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -2,8 +2,19 @@ import React, {Component} from 'react';
 import Chance from 'chance';
 
 class Detail extends Component {
+  buttonClicked() {
+    this.forceUpdate();
+  }
+
+
   render() {
-    return <p>Hello, {chance.first() + " " + chance.last()}</p>;
+    return (
+    <div>
+      <p>Hello, {chance.first()}.</p>
+      <p>You're from {chance.country({full: true})}.</p>
+      <button onClick={this.buttonClicked.bind(this)}>Meet Someone New!</button>
+    </div>
+    )
   }
 }
 
